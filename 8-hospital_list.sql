4c6839786a537e5cff5267b785002b4fc34a8c91
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-11-20 12:01:30 -0500

    Add author and license information to SQL file
    Added author information and licensing details.

--- 8-hospital_list.sql
+++ 8-hospital_list.sql
@@ -1,8 +1,12 @@
-
+/*
+Author:  Peter Leese
+© 2025, The University of North Carolina at Chapel Hill. The code is licensed under the MIT license and permission is granted to use in accordance with the MIT license.
+*/
 
 CREATE TABLE hospital_list AS
 SELECT macrovisit_id, 
 1 as likely_hospitalization
 FROM macro_filter
 where (drg is not null or max_cms_inpt=1 or max_resources>=50 or all_inpt=1 or all_icu=1)
+
 
