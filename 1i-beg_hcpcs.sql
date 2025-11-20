5b53fe2f6c678d2fc9f7840fc5b4d714867ca824
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-11-20 13:39:59 -0500

    license added

--- 1i-beg_hcpcs.sql
+++ 1i-beg_hcpcs.sql
@@ -1,4 +1,7 @@
-
+/*
+Author:  Peter Leese
+© 2025, The University of North Carolina at Chapel Hill. The code is licensed under the MIT license and permission is granted to use in accordance with the MIT license.
+*/
     --for sites that put hcpcs at beginning of strings
 
 CREATE TABLE beg_hcpcs AS
@@ -10,3 +13,4 @@ where data_partner_id in ( )
 
 ) a 
 where length(hcpc)=5
+
