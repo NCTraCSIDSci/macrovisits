c878cdcaf03555043453f7a987d00762d6bf1233
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-11-20 13:34:38 -0500

    Uncomment SQL code for identifying COVID visits

--- 1d-visit_with_covid.sql
+++ 1d-visit_with_covid.sql
@@ -1,3 +1,7 @@
+/*
+Author:  Peter Leese
+© 2025, The University of North Carolina at Chapel Hill. The code is licensed under the MIT license and permission is granted to use in accordance with the MIT license.
+*/
 
 --identify visits with likely covid diagnosis from concepts
 --this is a very quick and dirty method of identifying covid concepts but is sufficient for a fast inpt flag in macrovisit_fact
@@ -7,3 +11,4 @@ CREATE TABLE visit_with_covid AS
 SELECT 1 as covid_dx, *
 FROM omop.condition_occurrence
 where lower(condition_concept_name) like ('%cov%')
+
