5be89d2d0141577800edc589ea987280418d936f
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-11-20 13:37:14 -0500

    add license
    Added author information and licensing details to the SQL file.

--- 4b-inpt_hcpc_list.sql
+++ 4b-inpt_hcpc_list.sql
@@ -1,4 +1,9 @@
+/*
+Author:  Peter Leese
+© 2025, The University of North Carolina at Chapel Hill. The code is licensed under the MIT license and permission is granted to use in accordance with the MIT license.
+*/
 
 CREATE TABLE inpt_hcpc_list AS
 SELECT distinct visit_occurrence_id, cms_inpt
 FROM cms_inpt_hcpcs
+
