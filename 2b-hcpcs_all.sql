c9747db0f6ab63e67f96af1260338a82e9e058f8
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-11-20 13:41:35 -0500

    Update 2b-hcpcs_all.sql

--- 2b-hcpcs_all.sql
+++ 2b-hcpcs_all.sql
@@ -1,3 +1,7 @@
+/*
+Author:  Peter Leese
+© 2025, The University of North Carolina at Chapel Hill. The code is licensed under the MIT license and permission is granted to use in accordance with the MIT license.
+*/
 
 --combine extracted hcpcs data back together minus places where hcpc is null or empty
 
@@ -15,3 +19,4 @@ UNION
 
 select * from end_hcpcs
 where length(hcpc)>0
+
