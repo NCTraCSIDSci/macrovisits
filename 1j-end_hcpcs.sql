49f03092438932e9c5258fbb208b82359627e888
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-11-20 13:40:10 -0500

    Update 1j-end_hcpcs.sql

--- 1j-end_hcpcs.sql
+++ 1j-end_hcpcs.sql
@@ -1,3 +1,7 @@
+/*
+Author:  Peter Leese
+© 2025, The University of North Carolina at Chapel Hill. The code is licensed under the MIT license and permission is granted to use in accordance with the MIT license.
+*/
 
 --for sites that put hcpcs at end of strings
 
@@ -10,3 +14,4 @@ where data_partner_id in ( )
 
 ) A
 where length(hcpc)=5
+
