187e6779fea84a21abc9347244d8e79b72430598
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-11-20 13:38:46 -0500

    license added

--- 1e-dx_count.sql
+++ 1e-dx_count.sql
@@ -1,3 +1,7 @@
+/*
+Author:  Peter Leese
+© 2025, The University of North Carolina at Chapel Hill. The code is licensed under the MIT license and permission is granted to use in accordance with the MIT license.
+*/
 
 --count diagnoses for each visit
 
@@ -9,3 +13,4 @@ SELECT visit_occurrence_id,
  max(condition_start_date) as max_dx_date
 FROM omop.condition_occurrence
 group by visit_occurrence_id, person_id
+
