e0dc2036a7bd10c26b559ed4d0a9bebe43e5a291
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-11-20 13:34:25 -0500

    Add author information and license to SQL file

--- 1c-meas_count.sql
+++ 1c-meas_count.sql
@@ -1,3 +1,7 @@
+/*
+Author:  Peter Leese
+© 2025, The University of North Carolina at Chapel Hill. The code is licensed under the MIT license and permission is granted to use in accordance with the MIT license.
+*/
 
     --count measures for each visit
 
@@ -7,3 +11,4 @@ SELECT visit_occurrence_id, count(measurement_id) as meas_count
 FROM omop.measurements
 where visit_occurrence_id is not null
 group by visit_occurrence_id
+
