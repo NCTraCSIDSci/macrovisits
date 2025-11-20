ec4f81b7b1a1138a3765fc75c368c2790bac72dd
Peter Leese <47152964+peleese@users.noreply.github.com>
2025-11-20 13:39:34 -0500

    license added

--- 1h-hcpcs.sql
+++ 1h-hcpcs.sql
@@ -1,3 +1,7 @@
+/*
+Author:  Peter Leese
+© 2025, The University of North Carolina at Chapel Hill. The code is licensed under the MIT license and permission is granted to use in accordance with the MIT license.
+*/
 
 --first regex to identify hcpcs -- for 'cleaner', baseline hcpcs
 --exclude list of sites submitting data with low hcpcs quality 
@@ -7,4 +11,5 @@ SELECT *,
       regexp_extract(replace(replace(procedure_source_value,'CPT:'),'CPT4:',''), '(^[A-Z][0-9]{1,5}$|^[0-9]{5}$)') as hcpc
 FROM omop.procedure_occurrence
 where data_partner_id not in (    )
+
 
